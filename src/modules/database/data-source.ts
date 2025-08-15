5b930b3cd15cdaa412c65d43c9f75b18736f77dc
Julio Herrera <cesarchavarria0@gmail.com>
2025-08-14 22:12:15 -0600

    Fix Typeorm configuration

--- src/modules/database/data-source.ts
+++ src/modules/database/data-source.ts
@@ -4,11 +4,11 @@ import 'dotenv/config'; // loads .env automatically
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.DATABASE_HOST,
-  port: parseInt(process.env.DATABASE_PORT ?? '5432'),
+  port: parseInt(process.env.DATABASE_PORT || '5432'),
   username: process.env.DATABASE_USER,
   password: process.env.DATABASE_PASSWORD,
   database: process.env.DATABASE_NAME,
-  entities: ['src/modules/**/*.entity{.ts,.js}'],
-  migrations: ['src/modules/database/migrations/*.ts'],
+  entities: ['dist/modules/**/*.entity.js'],
+  migrations: ['dist/modules/database/migrations/*.js'],
   synchronize: false,
 });
